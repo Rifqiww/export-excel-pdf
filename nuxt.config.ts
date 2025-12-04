67bc00e0876f38d77ff503e54c7098c22a61e186
Rifqiww <rifqirafiudin569@gmail.com>
2025-12-04 08:41:29 +0700

    📦Add Package : pinia, primevue, and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,29 @@
+import Aura from "@primevue/themes/aura";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  css: ["~/assets/styles/main.css", "primeicons/primeicons.css"],
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: false },
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@primevue/nuxt-module",
+  ],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false || "none",
+        },
+      },
+    },
+  },
+});
